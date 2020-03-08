a0b744fcb4932dacebc262ff71a8881953d8d7a8
Alex <albece.95@gmail.com>
2020-03-08 16:44:20 +0100

    Some updates

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.simplecalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    
+    private Button  button1, button2, button3, button4, button5, button6, button7, buttonComma,
+            button8, button9, button0, buttonEqual, buttonSum, buttonSubtract, buttonMultiply, buttonDivide;
+    private String operation;
 
+    TextView mScreen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Declaration of all the widgets.
+        button0 = findViewById(R.id.button0);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        button5 = findViewById(R.id.button5);
+        button6 = findViewById(R.id.button6);
+        button7 = findViewById(R.id.button7);
+        button8 = findViewById(R.id.button8);
+        button9 = findViewById(R.id.button9);
+        buttonComma = findViewById(R.id.buttonComma);
+        buttonEqual = findViewById(R.id.buttonEqual);
+        buttonSum = findViewById(R.id.buttonSum);
+        buttonSubtract = findViewById(R.id.buttonSubtract);
+        buttonMultiply = findViewById(R.id.buttonMulti);
+        buttonDivide = findViewById(R.id.buttonDivide);
+        mScreen = findViewById(R.id.mScreen);
+        
+        
+        
     }
 }
